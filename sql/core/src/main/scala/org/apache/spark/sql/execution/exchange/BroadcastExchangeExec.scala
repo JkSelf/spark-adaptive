9f2c2f69157b7dc8fab46be6637c355df8028440
jiake <ke.a.jia@intel.com>
2019-05-29 16:43:38 +0800

    call the GC API before building the hash table to free more memory and prevent the OOM when building the hash table

--- sql/core/src/main/scala/org/apache/spark/sql/execution/exchange/BroadcastExchangeExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/exchange/BroadcastExchangeExec.scala
@@ -85,6 +85,10 @@ case class BroadcastExchangeExec(
           val beforeBuild = System.nanoTime()
           longMetric("collectTime") += (beforeBuild - beforeCollect) / 1000000
 
+          // Call the GC method to free more available memory
+          // and then prevent the oom issue when build the hash table
+          System.gc()
+
           // Construct the relation.
           val relation = mode.transform(input, Some(numRows), estimatedPageSize)
 
